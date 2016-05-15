e92bff1bbe8d3ba214e4a7266474d3656b24b5b2
alexxx-ivanow <alexxx-ivanow@yandex.ru>
2016-05-15 13:07:55 +0400

    Соединил слайдер с верхним меню и поменял эффект на кроссфейд

--- js/top-menu.js
+++ js/top-menu.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     
 	$(".but-close").click(function(){
 		$(".sub-ddd").slideUp("slow");
-		 $("#ddmenu li").RemoveClass("active");
+		 $("#ddmenu li").removeClass("active");
 	 });
 	 $("#ddmenu li a").click(function(){
         $(".sub-ddd").slideDown("slow");
